29adbcff8c00e5e8b7bf41fd6a7c162930922fbe
Daisuke <skd.itkn@gmail.com>
2023-12-30 20:33:00 -0800

    cmt

--- api/form.php
+++ api/form.php
@@ -4,18 +4,7 @@ $name=$_POST['userName'];
 $email=$_POST['email'];
 $inquiry=$_POST['inquiry'];
 
-
 function db_open(){
-    // $user='id21668771_phpuser';
-    // $password='Nira_1226';
-    // $opt=[
-    //     PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-    //     PDO::ATTR_EMULATE_PREPARES=>false,
-    //     PDO::MYSQL_ATTR_MULTI_STATEMENTS=>false,
-    // ];
-    // $dbh=new PDO('mysql:host=localhost;dbname=id21668771_forform',$user,$password,$opt);
-    // return $dbh;
-
     $user='sql3673292';
     $password='6IXqJTVStm';
     $opt=[
@@ -25,33 +14,24 @@ function db_open(){
     ];
     $dbh=new PDO('mysql:host=sql3.freesqldatabase.com;dbname=sql3673292',$user,$password,$opt);
     return $dbh;
-    }
+};
 
-
-
-    try{
-        $dbh=db_open();
+try{
+    $dbh=db_open();
+    $sql='insert into Form (id, name, email, inquiry) values (null, :name, :email, :inquiry)';
+    $stmt2=$dbh->prepare($sql);
     
+    $stmt2->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt2->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt2->bindParam(':inquiry', $inquiry, PDO::PARAM_STR);
     
-        $sql='insert into Form (id, name, email, inquiry) values (null, :name, :email, :inquiry)';
-        $stmt2=$dbh->prepare($sql);
-        //var_dump($stmt);
-        
-        $stmt2->bindParam(':name', $name, PDO::PARAM_STR);
-        $stmt2->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt2->bindParam(':inquiry', $inquiry, PDO::PARAM_STR);
-        
-        $stmt2->execute();
+    $stmt2->execute();
 
-        // echo '<script>alert("Inquiry has been successfully sent.")</script>';
-        echo '<script>location.href="https://portfolio-daisuke.vercel.app?inquiry"</script>';
-        
-        
-        
-    }catch(PDOException $e){
-        echo 'Error: ';
-        exit;
-    }
-
+    echo '<script>location.href="https://portfolio-daisuke.vercel.app?inquiry"</script>';
+    
+}catch(PDOException $e){
+    echo 'Error: ';
+    exit;
+};
 
 ?>
