447b32563d77fa93056f87e172fef120c5346602
Daisuke <skd.itkn@gmail.com>
2024-03-09 01:50:23 -0800

    video added

--- src/components/page/Works/WorkDetail.tsx
+++ src/components/page/Works/WorkDetail.tsx
@@ -12,12 +12,12 @@ import Github from '../../icons/Github';
 import Laptop from '../../icons/Laptop';
 
 import portfolio from '../../../../public/portfolio.png';
-// import ccc from '../../../../public/videos/Color Code Converter.mp4'
-// import ecommerce from '../../../../public/videos/Fake Shop.mp4'
-// import satellite from '../../../../public/videos/Satellite Launch simulation.mp4'
-// import animation from '../../../../public/videos/SVG Animation Gallery.mp4'
-// import memolis from '../../../../public/videos/memolis.mp4'
-// import dnn from '../../../../public/videos/DNN.mp4'
+import ccc from '../../../../public/videos/Color Code Converter.mp4'
+import ecommerce from '../../../../public/videos/Fake Shop.mp4'
+import satellite from '../../../../public/videos/Satellite Launch simulation.mp4'
+import animation from '../../../../public/videos/SVG Animation Gallery.mp4'
+import memolis from '../../../../public/videos/memolis.mp4'
+import dnn from '../../../../public/videos/DNN.mp4'
 
 // import { Selected } from '../../../App';
 // import js from './works.json';
@@ -41,12 +41,12 @@ function WorkDetail(props:{showDetail:showDetailType, setShowDetail:React.Dispat
   const languageObj:{[key:string]:JSX.Element} = {'JS':<JS/>,'TS':<TS/>,'Jquery':<Jquery strokeColor={'#000000'}/>,'React':<React/>,'MySQL':<MySQL/>,'PHP':<PHP/>,'PostgreSQL':<PostgreSQL strokeColor={'#000000'}/>,'NodeJs':<NodeJS2/>}
   // const paths:Array<string> = [memolis, ccc, ecommerce, satellite, animation, news, portfolio]
   const videoPaths:{[key:string]:string} = {
-    // 'memolis':memolis,
-    // 'Color Code Converter':ccc,
-    // 'E-Commerce':ecommerce,
-    // 'Satellite Launch Simulation':satellite,
-    // 'SVG Animation Gallery':animation,
-    // 'DNN':dnn
+    'memolis':memolis,
+    'Color Code Converter':ccc,
+    'E-Commerce':ecommerce,
+    'Satellite Launch Simulation':satellite,
+    'SVG Animation Gallery':animation,
+    'DNN':dnn
   }
 
 
