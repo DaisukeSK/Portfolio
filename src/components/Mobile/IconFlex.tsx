e221e96c86092fcc3cd5fb14a7776a53733fa9ff
Daisuke <skd.itkn@gmail.com>
2024-08-14 03:14:16 -0700

    mobile page updated

--- src/components/Mobile/IconFlex.tsx
+++ src/components/Mobile/IconFlex.tsx
@@ -5,7 +5,7 @@ function IconFlex(props:{icons:Array<string>, width?:number|undefined}) {
     return (
         <div className='icons' style={{width:props.width && `${props.width}px`}}>
 
-            {props.icons.map((icon:string, key:number)=>{
+            {props.icons.map((icon:string)=>{
                 return iconObj[icon]
             })}
 
